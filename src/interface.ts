be012f0a52e5c387cce3845ca9af30b49385392a
jamesjianpeng <jamesjianpeng@gmail.com>
2020-09-10 00:13:56 +0800

    feat: 添加 uploader interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,3 +1,34 @@
-export interface test {
+export namespace IUploader {
+   export interface Options {
+    el?: HTMLElement
+    id?: string
+    action: string,
+    before: (file: File) => Promise<boolean>
+    success: (data: Response) => void
+  }
 
+  export interface Response {
+    msg: string,
+    code: number
+  }
+
+  export interface Meta {
+    width?: string
+    height?: string
+    proportion?: string
+  }
+
+  export interface ImageMeta extends Meta {}
+  export interface VideoMeta extends Meta {}
+}
+
+export interface IUploaderClass {
+  options: IUploader.Options
+  inputEl: HTMLInputElement | null
+  before: (file: File, meta: IUploader.Meta) => Promise<boolean>
+  success: (data: IUploader.Response) => Promise<void>
+  getImageSize: (file: File) => Promise<IUploader.ImageMeta>
+  getVideoSize: (file: File) => Promise<IUploader.VideoMeta>
+  createInput: (id: string | 'Uploader') => HTMLInputElement
+  createHiddenInput: (id: string | 'Uploader') => HTMLInputElement
 }
