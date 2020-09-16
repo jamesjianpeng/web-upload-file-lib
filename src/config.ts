772d50815bce2eb7e70171372d9a4d7b5f434893
jamesjianpeng <jamesjianpeng@gmail.com>
2020-09-16 10:07:57 +0800

    perf: 添加 MIME 类型配置

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+const MIME_TYPE = {
+  "css": "text/css",
+  "gif": "image/gif",
+  "html": "text/html",
+  "ico": "image/x-icon",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpg",
+  "js": "text/javascript",
+  "json": "application/json",
+  "pdf": "application/pdf",
+  "png": "image/png",
+  "svg": "image/svg+xml",
+  "swf": "application/x-shockwave-flash",
+  "tiff": "image/tiff",
+  "txt": "text/plain",
+  "wav": "audio/x-wav",
+  "wma": "audio/x-ms-wma",
+  "wmv": "video/x-ms-wmv",
+  "xml": "text/xml",
+  "xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  "doc": "application/msword",
+  "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+  "mp4": "video/mp4"
+}
+
+export {
+  MIME_TYPE
+}
