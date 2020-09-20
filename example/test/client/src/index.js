33ee4f0a493748bfe308267e105f48f1f2ff001b
jamesjianpeng <jamesjianpeng@gmail.com>
2020-09-20 23:17:16 +0800

    perf: 添加测试方法

--- example/test/client/src/index.js
+++ example/test/client/src/index.js
@@ -2,6 +2,7 @@ import { Uploader } from './dist/index.js'
 new Uploader({
   el: document.querySelector('#upload'),
   action: 'http://localhost:3400/upload',
+  oneSlice: 1 * 1024 * 1024,
   before (files) {
     console.log(files)
     return true
