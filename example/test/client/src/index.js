f38e9656d2c4f4a12d54d43e71f78f2a47265d72
jamesjianpeng <jamesjianpeng@gmail.com>
2020-09-22 13:27:09 +0800

    perf: 添加测试代码

--- example/test/client/src/index.js
+++ example/test/client/src/index.js
@@ -1,17 +1,29 @@
 import { Uploader } from './dist/index.js'
+let urls = []
 new Uploader({
   el: document.querySelector('#upload'),
   action: 'http://localhost:3400/upload',
-  oneSlice: 1 * 1024 * 1024,
+  oneSlice: 20 * 1024, // 20 kb
   before (files) {
     console.log(files)
+    urls = []
+    return true
+  },
+  mergeShardFileBefore (data) {
+    console.log('before')
+    console.log(data)
     return true
   },
   progress (xhr, arg) {
     console.log('start')
     console.log(xhr)
+    urls.push(xhr.response.url)
     console.log(arg)
     console.log('end')
+  },
+  mergeShardFile (urls) {
+    console.log('before')
+    console.log(urls)
   },
   response (xhr, arg) {
     console.log(xhr)
