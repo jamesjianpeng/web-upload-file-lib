765f7402704fee0f2b5a852d1efa0f845239f040
jamesjianpeng <jamesjianpeng@gmail.com>
2020-09-24 17:59:56 +0800

    refactor: 添加测试代码

--- example/test/client/src/index.js
+++ example/test/client/src/index.js
@@ -1,19 +1,14 @@
 import { Uploader } from './dist/index.js'
-import axios from 'axios'
-const oneSlice = 20 * 1024
 let urls = []
-new Uploader({
+const instance = new Uploader({
   el: document.querySelector('#upload'),
   action: 'http://localhost:3400/upload',
-  oneSlice, // 20 kb
+  inputProps: {
+    multiple: true
+  },
   before (files) {
     console.log(files)
     urls = []
-    return true
-  },
-  uploadShardFileBefore (data) {
-    console.log('before')
-    console.log(data)
     return true
   },
   progress (xhr, arg) {
@@ -24,16 +19,40 @@ new Uploader({
       }
     }
   },
-  async mergeShardFileBefore (arg) {
-    console.log('mergeShardFileBefore')
-    console.log(urls)
+  response (xhr, arg) {
+    console.log(xhr)
     console.log(arg)
-    const res = await axios.post('http://127.0.0.1:3400/mergeShardFile', { urls, name: 'mergetest.png', oneSlice })
-    console.log(res)
+  }
+})
+console.log(instance)
+// setTimeout(() => {
+const instance2 = new Uploader({
+  el: document.querySelector('#upload2'),
+  action: 'http://localhost:3400/upload',
+  inputProps: {
+    multiple: true
+  },
+  before (files) {
+    console.log(files)
+    urls = []
     return true
+  },
+  progress (xhr, arg) {
+    if (arg.shard) {
+      const url = JSON.parse(xhr.response)[0].url
+      if (xhr.response) {
+        urls.push(url)
+      }
+    }
   },
   response (xhr, arg) {
     console.log(xhr)
     console.log(arg)
   }
 })
+console.log(instance2)
+  
+// }, 2000)
+document.querySelector('#upload2_remove').onclick = () => {
+  instance2.removeTriggerEvent()
+}
