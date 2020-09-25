1c27493e7ff4b601a0b4bc1df1ddd07ed0cc91e9
jamesjianpeng <jamesjianpeng@gmail.com>
2020-09-25 11:39:17 +0800

    perf: dist , 更新 .git , .npmignore

--- dist/src/core/xhr.d.ts
+++
@@ -1,3 +0,0 @@
-declare const request: (options: any) => Promise<unknown>;
-declare const uploadFormData: (action: string, formData: FormData) => Promise<unknown>;
-export { request, uploadFormData };
